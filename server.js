c4aa83a140be208d65bfac02b25111717935c9bb
Jaakko Juutila <jaakko.juutila@gmail.com>
2015-07-31 13:24:51 +0300

    Add server start logging

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var static = require('node-static');
 var http = require('http');
 
+var PORT = 8080;
 var fileServer = new static.Server('./public');
 
 http.createServer(function (request, response) {
   request.addListener('end', function () {
     fileServer.serve(request, response);
   }).resume();
-}).listen(8080);
+}).listen(PORT);
+
+console.log('pradiator server started at http://localhost:' + PORT + '/');
