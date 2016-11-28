e4be9665ba7bf5e1824acc75358a27f13624d790
Jaakko Juutila <jaakko.juutila@gmail.com>
2016-11-28 13:53:12 +0200

    Wrap code to a strict closure function

--- public/index.js
+++ public/index.js
@@ -1,58 +1,75 @@
-var createPullRequestListHtml = R.pipe(R.sort(compareDate), R.map(pullRequestsToHtml));
+(function () {
+    'use strict';
 
-var apiResponses = Bacon.combineAsArray(getPullRequests(), getDomReadyStream())
-  .map(R.prop(0));
+    var createPullRequestListHtml = R.pipe(R.sort(compareDate), R.map(pullRequestsToHtml));
 
-apiResponses.onValue(showResults);
-apiResponses.onError(showError);
+    var apiResponses = Bacon.combineAsArray(getPullRequests(), getDomReadyStream())
+        .map(R.prop(0));
 
-function compareDate(a, b) {
-  return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
-}
+    apiResponses.onValue(showResults);
+    apiResponses.onError(showError);
 
-function pullRequestsToHtml(pr) {
-  return $('<div>', {class: 'pull-request'})
-    .append($('<span>', {class: 'repository', text: pr.repository}))
-    .append($('<a>', {class: 'pr-title', href: pr.html_url, target: '_blank', text: pr.title}))
-    .append($('<div>', {class: 'meta', text: getMetaText(pr)}));
-}
+    function compareDate(a, b) {
+        return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
+    }
 
-function getMetaText(pr) {
-  return '#' + pr.number + ' opened ' + moment(pr.created_at).fromNow() + ' by ' + pr.user.login;
-}
+    function pullRequestsToHtml(pr) {
+        return $('<div>', {
+                class: 'pull-request'
+            })
+            .append($('<span>', {
+                class: 'repository',
+                text: pr.repository
+            }))
+            .append($('<a>', {
+                class: 'pr-title',
+                href: pr.html_url,
+                target: '_blank',
+                text: pr.title
+            }))
+            .append($('<div>', {
+                class: 'meta',
+                text: getMetaText(pr)
+            }));
+    }
 
-function setSpinning(isSpinning) {
-  if (isSpinning) {
-    $('.github-logo').addClass('spinning');
-  } else {
-    $('.github-logo').removeClass('spinning');
-  }
-}
+    function getMetaText(pr) {
+        return '#' + pr.number + ' opened ' + moment(pr.created_at).fromNow() + ' by ' + pr.user.login;
+    }
 
-function showResults(results) {
-  var pullRequestHtml = createPullRequestListHtml(results);
-  $('#pull-requests').html(pullRequestHtml);
+    function setSpinning(isSpinning) {
+        if (isSpinning) {
+            $('.github-logo').addClass('spinning');
+        } else {
+            $('.github-logo').removeClass('spinning');
+        }
+    }
 
-  setSpinning(false);
-}
+    function showResults(results) {
+        var pullRequestHtml = createPullRequestListHtml(results);
+        $('#pull-requests').html(pullRequestHtml);
 
-function showError(error) {
-  setSpinning(false);
-  console.log('ERROR', error);
-}
+        setSpinning(false);
+    }
 
-function getDomReadyStream() {
-  setSpinning(true);
-  return Bacon.fromCallback(function(callback) {
-    $(document).ready(callback);
-  });
-}
+    function showError(error) {
+        setSpinning(false);
+        console.log('ERROR', error);
+    }
 
-function getPullRequests() {
-    return Bacon.fromPromise($.ajax({
-        url: '/prs/',
-        type: 'GET',
-        dataType: 'json',
-        timeout: 10000
-    }));
-}
+    function getDomReadyStream() {
+        setSpinning(true);
+        return Bacon.fromCallback(function (callback) {
+            $(document).ready(callback);
+        });
+    }
+
+    function getPullRequests() {
+        return Bacon.fromPromise($.ajax({
+            url: '/prs/',
+            type: 'GET',
+            dataType: 'json',
+            timeout: 10000
+        }));
+    }
+}());
